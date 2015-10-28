5e04a652a105f4f7f296d9fde8fa847db25e6181
patricksandquist <patricksandquist@gmail.com>
2015-10-28 10:04:29 -0700

    Add board class

--- snake.js
+++ snake.js
@@ -4,6 +4,7 @@
   }
 
   var Coord = Game.Coord = function (i, j) {
+    // A coordinate class
     this.i = i;
     this.j = j;
   };
@@ -20,9 +21,62 @@
     return new Cord(this.i + coord2.i, this.j + coord2.j);
   };
 
-  var Snake = Game.Snake = function () {
-    this.dir = "N";
+  var Board = Game.Board = function (dim) {
+    this.dim = dim;
+    this.snake = new Snake();
+    this.apple = new Apple();
+  };
 
-    this.segments = [];
+  Board.BLANK_SYMBOL = ".";
+
+  Board.prototype.blankGrid = function () {
+    var grid = [];
+
+    for (var i = 0; i < this.dim; i++) {
+      var row = [];
+      for (var j = 0; j < this.dim; j++) {
+        row.push(this.BLANK_SYMBOL);
+      }
+      grid.push(row);
+    }
+
+    return grid;
+  };
+
+  Board.prototype.render = function () {
+    var grid = this.blankGrid();
+    this.snake.segments.forEach(function (segment) {
+      grid[segment.i][segment.j] = Snake.SYMBOL;
+    });
+  };
+
+  var Snake = Game.Snake = function () {
+    // A Snake class that stores the current direction and an array of segments
+    this.dir = "N";
+    this.segments = [new Cord(5, 5)];
+  };
+
+  Snake.SYMBOL = "S";
+
+  Snake.DIFFS = {
+    // The coordinate differences for each direction of movement
+    "N": new Coord(0, -1),
+    "E": new Coord(1, 0),
+    "S": new Coord(0, 1),
+    "W": new Coord(-1, 0)
+  };
+
+  Snake.prototype.head = function () {
+    // Returns the head coordinate object
+    return this.segments[this.segments.length - 1];
+  };
+
+  Snake.prototype.move = function () {
+    // move snake forward
+    this.segments.push(this.head().plus(Snake.DIFFS[this.dir]));
+  };
+
+  Snake.prototype.turn = function (newDir) {
+    this.dir = newDir;
   };
 })();
