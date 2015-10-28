563e1c6b3f8f7b1c13351c3743ab7cc87119958f
patricksandquist <patricksandquist@gmail.com>
2015-10-28 09:24:39 -0700

    Initial commit

---
+++ snake.js
@@ -0,0 +1,28 @@
+(function () {
+  if (typeof Game === "undefined") {
+    window.Game = {};
+  }
+
+  var Coord = Game.Coord = function (i, j) {
+    this.i = i;
+    this.j = j;
+  };
+
+  Coord.prototype.equals = function (coord2) {
+    return (this.i == coord2.i && this.j == coord2.j);
+  };
+
+  Coord.prototype.isOpposite = function (coord2) {
+    return (this.i == (-1 * coord2.i) && this.j == (-1 * coord2.j));
+  };
+
+  Coord.prototype.plus = function (coord2) {
+    return new Cord(this.i + coord2.i, this.j + coord2.j);
+  };
+
+  var Snake = Game.Snake = function () {
+    this.dir = "N";
+
+    this.segments = [];
+  };
+})();
