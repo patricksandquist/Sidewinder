cf2d6434cf07dc76c8d7cf5ee8a326ee54656ed2
Patrick Sandquist <patricksandquist@gmail.com>
2015-11-04 16:12:42 -0800

    Alter speeds

--- js/snakeView.js
+++ js/snakeView.js
@@ -24,8 +24,8 @@
     37: "W"
   };
 
-  View.FAST_DELAY = 100;
-  View.SLOW_DELAY = 200;
+  View.FAST_DELAY = 60;
+  View.SLOW_DELAY = 120;
   View.SPIN_DELAY = 6000;
 
   View.prototype.handleKeyEvent = function (e) {
